677319c5a68ca7d85654f93deedd103f079f7afa
Thomascogez <thomas.cogez@gmail.com>
2021-04-13 20:05:34 +0200

    register route

--- packages/common/src/schemas/users/userRegisterSchema.ts
+++ packages/common/src/schemas/users/userRegisterSchema.ts
@@ -4,6 +4,6 @@ import { REQUIRED_ERROR_MESSAGE, INVALID_EMAIL_ERROR_MESSAGE } from '../../const
 export const userRegisterSchema = yup.object().shape({
     userName: yup.string().required(REQUIRED_ERROR_MESSAGE).min(3, "Le nom doit faire un minimum de 4 caractères"),
     userEmail: yup.string().email(INVALID_EMAIL_ERROR_MESSAGE).required(REQUIRED_ERROR_MESSAGE),
-    userPassword: yup.string().required(REQUIRED_ERROR_MESSAGE).matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, "Le mot de passe doit contenir un caractère spécial, une lettre majuscule, minuscule et un chiffre"),
+    userPassword: yup.string().required(REQUIRED_ERROR_MESSAGE).matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, "Le mot de passe doit contenir un caractère spécial, une lettre majuscule, minuscule, chiffre et doit faire au minimum 8 caractère"),
     userConfirmPassword: yup.string().oneOf([yup.ref('userPassword'), undefined], "Les mots ne correspondent pas").required(REQUIRED_ERROR_MESSAGE),
 })
--- packages/tchather_api/src/controllers/UserController.ts
+++ packages/tchather_api/src/controllers/UserController.ts
@@ -1,19 +1,35 @@
-import { BadRequestError, Body, JsonController, Post } from 'routing-controllers';
+import {
+  BadRequestError,
+  Body,
+  JsonController,
+  Post,
+} from "routing-controllers";
+import { userRegisterSchema } from "@tchather/common";
+import { Asserts } from "yup";
+import { hash } from "bcrypt";
 import { prismaClient } from "../utils/prismaClient";
-import { Asserts } from 'yup';
-import { userRegisterSchema } from '@tchather/common';
-
 @JsonController("/users")
 export class UserController {
-    @Post("/register")
-    async registerUser(@Body() body: Asserts<typeof userRegisterSchema>) {
-        try {
-            await userRegisterSchema.validate(body, {abortEarly: false});
-        } catch (error) {
-            throw new BadRequestError(error);
-        }
-        
-        
-        await prismaClient.user.create({data:body});
+  @Post("/register")
+  async registerUser(@Body() body: Asserts<typeof userRegisterSchema>) {
+    try {
+      await userRegisterSchema.validate(body, { abortEarly: false });
+    } catch (error) {
+      throw new BadRequestError(error);
     }
+    const { userPassword, userConfirmPassword, ...user } = body;
+    const hashedPassword = await hash(userPassword, 12);
+
+    const savedUser = await prismaClient.user.create({
+      data: { ...user, userPassword: hashedPassword },
+      select: {
+        userId: true,
+        userConversations: true,
+        userEmail: true,
+        userName: true,
+      },
+    });
+
+    return savedUser;
+  }
 }
